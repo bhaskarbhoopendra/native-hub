45177a994003b06140bb3cff0d00751b8e6af3b5
bhaskarbhoopendra <bhaskarbhoopendra8@gmail.com>
2022-12-27 16:36:18 +0530

    adding grocer

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,50 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet, Image, ScrollView, SafeAreaView, TextInput} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const apiUrl = `https://api.github.com/users`
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const githubUsers = async()=>{
+    const {data} = await axios.get(apiUrl)
+    setUsers(data)
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    githubUsers()
+  },[])
+
+  const handleSearch = (search:string)=>{
+    const filteredUsers = users.filter((item:any)=> item.login.toLowerCase().includes(search.toLowerCase()))
+    setUsers(filteredUsers)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <Text style={styles.title}>Github Users</Text>
+          <TextInput
+              style={styles.input}
+              placeholder="Search"
+              onChangeText={(newText)=> handleSearch(newText)}
+          />
+          {users.map((user:any)=>{
+            const {login,id, avatar_url} = user
+            return <View key={id}>
+              <Text>{login}</Text>
+              <Image source={{ uri: avatar_url }} style={{ width: 'auto', height: 150 }} />
+            </View>
+          })}
+
+        </ScrollView>
+      </SafeAreaView>
+
   );
 }
 
@@ -18,15 +52,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: 'bold',
+    marginBottom:10
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  stretch: {
+    width: 50,
+    height: 200,
+    resizeMode: 'stretch'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor:"#fff"
   },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,80 @@
-import { StyleSheet } from 'react-native';
+import {Button, Pressable, StyleSheet, TextInput} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import {useState} from "react";
 
 export default function TabTwoScreen() {
+  const [list, setList] = useState([]) as any;
+  const [search, setSearch] = useState('');
+  const [editId, setEditId] = useState(null) as any;
+  const [editing, setEditing] = useState(false);
+  const onPressLearnMore =()=>{
+    if (editId && editing) {
+      setList(
+          list.map((item: any) => {
+            if (item.id === editId) {
+              return { ...item, title: search };
+            }
+            return item;
+          })
+      );
+      setEditing(false);
+      setEditId(0);
+    }else{
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: search
+      }
+      setList([...list, newItem])
+      setSearch("")
+    }
+
+
+  }
+
+  const deleteOneItem = (id:number)=>{
+    const filteredItem = list.filter((item:any)=> item.id !== id)
+    setList(filteredItem)
+  }
+
+  const editOneItem = (id:number)=>{
+    list.map((item:any)=>{
+      if(item.id === id){
+        setSearch((item.title))
+      }
+    })
+    setEditing(true)
+    setEditId(id)
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text style={styles.title}>Grocer</Text>
+      <TextInput
+          style={styles.input}
+          onChangeText={(text:string) => setSearch(text)}
+          placeholder="e.g. Eggs"
+          value={search}
+      />
+      <View style={styles.pressable}>
+        <Button onPress={onPressLearnMore} color="#B33771" title="Add To List"></Button>
+      </View>
+
+      <View style={styles.listView}>
+
+        {list.map((item:any)=>{
+          return <View style={styles.inlineItem}>
+            <Text style={styles.listItem} key={item.id}> {item.title}</Text>
+            <View style={styles.editButton} >
+              <Button onPress={()=> editOneItem(item.id)} title={"Edit"} color={"#55E6C1"}/>
+            </View>
+            <View style={styles.editButton}>
+              <Button onPress={()=> deleteOneItem(item.id)} title={"Delete"} color={"#FC427B"}/>
+            </View>
+          </View>
+        })}
+      </View>
+
     </View>
   );
 }
@@ -17,10 +83,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal:16
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: 'bold',
   },
   separator: {
@@ -28,4 +94,40 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 5,
+    padding: 10,
+    backgroundColor:"#fff",
+    width:"80%"
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  pressable:{
+    width:"80%"
+  },
+  listView:{
+    flex:1,
+    alignItems:"flex-start",
+    marginTop:10
+  },
+  listItem:{
+    fontSize:25,
+
+  },
+  inlineItem:{
+    flex: 1,
+    flexDirection:"row",
+    alignItems:"baseline",
+
+  },
+  editButton:{
+    paddingRight:11,
+    paddingLeft:11
+  }
+
+
 });
